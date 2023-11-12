60585576c48581033cda24ae0f57d9947206b0df
Egor Kuziakov <egor.kuziakov@gmail.com>
2023-11-12 21:36:57 +0300

    Add body

---
+++ src/SyntaxAnalysis/Grammar/Body.java
@@ -0,0 +1,45 @@
+package SyntaxAnalysis.Grammar;
+
+import LexicalAnalysis.Tokens.SeparatorToken;
+import LexicalAnalysis.Tokens.Token;
+import SyntaxAnalysis.Grammar.Statements.Statement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Body extends Grammar {
+    List<Statement> statements;
+
+    public Body(int startToken, int tokensCount, List<Statement> statements) {
+        super(startToken, tokensCount);
+        this.statements = statements;
+    }
+
+    public static Body findNext(List<Token> tokens, int startToken) throws SyntaxError {
+        List<Statement> statements = new ArrayList<>();
+        int currentToken = startToken;
+        Statement statement;
+        do {
+            statement = Statement.findNext(tokens, currentToken);
+            if (statement != null) {
+                statements.add(statement);
+                currentToken += statement.getTokensCount();
+                Token token = tokens.get(currentToken);
+                if (token instanceof SeparatorToken && ((SeparatorToken) token).getSeparator().equals(";")) {
+                    currentToken++;
+                } else {
+                    return null;
+                }
+            }
+        } while (currentToken < tokens.size() && statement != null);
+        return new Body(startToken, currentToken - startToken, statements);
+    }
+
+    @Override
+    public Map<String, Object> getJSONFields() {
+        Map<String, Object> fields = super.getJSONFields();
+        fields.put("statements", statements);
+        return fields;
+    }
+}
